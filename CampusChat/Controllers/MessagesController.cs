5b7d91c9a34966d8f6f9a7b785deb379f7d9ba46
SarimK1994 <sarim@email.uscupstate.edu>
2018-10-17 14:32:22 -0400

    Upload solution to master

---
+++ CampusChat/Controllers/MessagesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CampusChat.Models;
+
+namespace CampusChat.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private CampusChatDatabaseEntities db = new CampusChatDatabaseEntities();
+
+        // GET: Messages
+        public ActionResult Index()
+        {
+            var messages = db.Messages.Include(m => m.User).Include(m => m.User1);
+            return View(messages.ToList());
+        }
+
+        // GET: Messages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // GET: Messages/Create
+        public ActionResult Create()
+        {
+            ViewBag.SenderID = new SelectList(db.Users, "UserID", "Username");
+            ViewBag.RecieverID = new SelectList(db.Users, "UserID", "Username");
+            return View();
+        }
+
+        // POST: Messages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MessageID,SenderID,RecieverID,Content,SentTime")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Messages.Add(message);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SenderID = new SelectList(db.Users, "UserID", "Username", message.SenderID);
+            ViewBag.RecieverID = new SelectList(db.Users, "UserID", "Username", message.RecieverID);
+            return View(message);
+        }
+
+        // GET: Messages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SenderID = new SelectList(db.Users, "UserID", "Username", message.SenderID);
+            ViewBag.RecieverID = new SelectList(db.Users, "UserID", "Username", message.RecieverID);
+            return View(message);
+        }
+
+        // POST: Messages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MessageID,SenderID,RecieverID,Content,SentTime")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(message).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SenderID = new SelectList(db.Users, "UserID", "Username", message.SenderID);
+            ViewBag.RecieverID = new SelectList(db.Users, "UserID", "Username", message.RecieverID);
+            return View(message);
+        }
+
+        // GET: Messages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // POST: Messages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Message message = db.Messages.Find(id);
+            db.Messages.Remove(message);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CampusChat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CampusChat.MvcApplication" Language="C#" %>
