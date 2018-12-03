0b484bb5e03853fc5363d25de2b61c95547919a1
Daniyal Farid <daniyal.farid14@hotmail.com>
2018-12-03 15:29:03 -0500

    Updated links for asp syntax instead of hrefs

--- CampusChat/Controllers/HomeController.cs
+++ CampusChat/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CampusChat.Models;
+using Microsoft.AspNet.Identity;
 
 namespace CampusChat.Controllers
 {
@@ -10,6 +12,10 @@ namespace CampusChat.Controllers
     {
         public ActionResult Index()
         {
+            if(User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Posts");
+            }
             return View();
         }
 
